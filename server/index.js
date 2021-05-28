75f516cdf1a74e5040b4001d3930eb0c8091010e
Serafet Sahin <serafet6767@gmail.com>
2021-05-28 22:47:39 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser : true, useUnifie
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
+
 mongoose.set('useFindAndModify', false);
 
 
